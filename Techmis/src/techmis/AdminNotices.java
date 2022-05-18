6bd8336f2e4aa6daf7aca1f4f548f79fde7890db
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2022-05-18 09:38:49 +0530

    add timetable interface & create classes

---
+++ Techmis/src/techmis/AdminNotices.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package techmis;
+
+/**
+ *
+ * @author ASUS
+ */
+public class AdminNotices {
+    
+    private String ID;
+    private String date;
+    private String title;
+    private String notice;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNotice() {
+        return notice;
+    }
+
+    public void setNotice(String notice) {
+        this.notice = notice;
+    }
+    
+    
+    
+}
